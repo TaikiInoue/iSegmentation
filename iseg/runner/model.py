26259c913ed306580fb12d0b6016e4532bd4d42b
TaikiInoue <taikiinoue45@gmail.com>
2020-09-27 12:20:17 +0000

    Fix bug

--- iseg/runner/model.py
+++ iseg/runner/model.py
@@ -1,6 +1,5 @@
 import iseg.models
 import iseg.types as T
-from omegaconf import OmegaConf
 
 
 class RunnerModel:
@@ -9,6 +8,5 @@ class RunnerModel:
 
     def init_model(self) -> T.Module:
 
-        model_cfg = OmegaConf.load(self.cfg.model.yaml)
         cls = getattr(iseg.models, self.cfg.model.name)
-        return cls(model_cfg)
+        return cls(self.cfg.model.yaml)
