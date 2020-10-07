206a9b90475aaf3ea4c6883482354b51a6c1bf1e
TaikiInoue <taikiinoue45@gmail.com>
2020-10-07 14:43:34 +0000

    Update donwload_cityscape.sh

--- data/cityscape/download_cityscape.sh
+++ data/cityscape/download_cityscape.sh
@@ -3,5 +3,7 @@ read CITYSPACES_USERNAME
 echo -n CITYSPACES_PASSWORD:
 read CITYSPACES_PASSWORD
 wget --keep-session-cookies --save-cookies=cookies.txt --post-data 'username='${CITYSPACES_USERNAME}'&password='${CITYSPACES_PASSWORD}'&submit=Login' https://www.cityscapes-dataset.com/login/
-wget --load-cookies cookies.txt --content-disposition https://www.cityscapes-dataset.com/file-handling/?packageID=1
-wget --load-cookies cookies.txt --content-disposition https://www.cityscapes-dataset.com/file-handling/?packageID=3
+wget --load-cookies cookies.txt --content-disposition https://www.cityscapes-dataset.com/file-handling/?packageID=1 -P data/cityscape
+wget --load-cookies cookies.txt --content-disposition https://www.cityscapes-dataset.com/file-handling/?packageID=3 -P data/cityscape
+unzip data/cityscape/gtFine_trainvaltest.zip
+unzip data/cityscape/leftImg8bit_trainvaltest.zip
